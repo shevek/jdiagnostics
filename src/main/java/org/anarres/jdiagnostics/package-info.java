765843835126feb0e88048d08ba9b41c10a41bd6
Shevek <shevek@anarres.org>
2017-04-19 16:07:49 -0700

    more javadoc

--- src/main/java/org/anarres/jdiagnostics/package-info.java
+++ src/main/java/org/anarres/jdiagnostics/package-info.java
@@ -5,13 +5,17 @@
  * {@link ProductMetadata} and {@link DefaultQuery Query}.
  *
  * <h2>ProductMetadata</h2>
- * ProductMetadata is an interface to build metadata stored in the JAR files
+ * ProductMetadata is an interface to read metadata stored in the JAR files
  * of all your projects. You can implement a --version command-line flag as
  * <code>
  * System.out.println(new ProductMetadata());
  * </code>
+ * You might want this to check, for example, that a customer is using a version
+ * of your product built on a particular date, to discover the git hashref it
+ * was build from, and so forth.
  * Of course it has a great many other accessors for querying available modules
- * and versions.
+ * and versions, and supports metadata from many sources, although our favourite
+ * is nebula-plugins/gradle-info-plugin.
  *
  * <h2>Query</h2>
  * Query and DefaultQuery are a failure-avoidant way to dump a great deal of
